6f67585dbe9582ae829024100b95edb9e8436f89
Stephan Michailenko <94607203+stephan-art@users.noreply.github.com>
2022-10-09 17:48:47 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿// Michailenko SA
+// Task 2
+// 09.10.2022
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int w;
+	cin >> w;
+	if (w % 2 == 0) {
+		// тело
+		// выполнится только есть w % 2 =
+		cout << "YES";
+	}
+	else {// только если w % 2 != 0
+		cout << "NO";
+	}
+
+}
